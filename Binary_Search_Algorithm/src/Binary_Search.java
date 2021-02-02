0d0b4b30778ef61af5980e2f64c12ac802c65644
JiSeungGu <tmdrn457@naver.com>
2021-02-02 15:59:54 +0900

    Binary_Search Algorithm

--- Binary_Search_Algorithm/src/Binary_Search.java
+++ Binary_Search_Algorithm/src/Binary_Search.java
@@ -1,4 +1,44 @@
+import java.util.Arrays;
 
 public class Binary_Search {
-
+	
+	public static void main(String[] args) {
+		
+		int[] arr = {1,4,8,12,18,20,23,26,30};
+		Arrays.sort(arr);
+		
+		System.out.println(binarySearch_recursion(arr, 0, arr.length-1, 4));
+		System.out.println(binarySearch_loop(arr, 0, arr.length-1, 23));
+		
+	}
+	
+	public static int binarySearch_recursion(int[] arr,int start,int last,int no) {
+		int mid = (int)(start+last)/2;
+		
+		if(arr[mid] == no) { 
+			return mid; //순서를 반환하기 위한 mid
+		}
+		else if(arr[mid] > no) {
+			last = mid - 1;
+			return binarySearch_recursion(arr,start,last,no);
+		}
+		else {
+			start = mid + 1;
+			return binarySearch_recursion(arr,start,last,no);
+		}
+	}
+	
+	public static int binarySearch_loop(int[] arr,int start,int last,int no) {
+		while(true) {
+			int mid = (int)(start+last)/2;
+			if(arr[mid]==no){
+				return mid;
+			} else if(arr[mid] > no) {
+				last = mid - 1;
+			} else {
+				start = mid + 1;
+			}
+		}
+	}
+	
 }
